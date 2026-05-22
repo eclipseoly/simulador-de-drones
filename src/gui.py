6b74f819d6e415ac42172dd33243b693eb4c0690
ecplise <cuzcuzcomqueijo2007@gmail.com>
2026-05-22 00:20:33 -0300

    pequenas alteracoes de teste

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,47 @@
-# drones circulando ao longo do tempo
-import tkinter as tk
-janela = tk.Tk()
-janela.title("Minha Primeira GUI")
-janela.geometry("400x300")
-janela.mainloop()
+import customtkinter as ctk
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Simulador de Drones")
+        self.geometry("800x600")
+        self.attributes("-fullscreen",True)
+        
+  
+        self.frame_topo = ctk.CTkFrame(
+            self,
+            height=40
+        )
+        self.frame_topo.pack(
+            side="top",
+            fill="x",
+            padx=10,
+            pady=5
+        )
+        self.frame_topo.pack_propagate(False)
+
+        self.botao_minimizar = ctk.CTkButton(
+            self.frame_topo,
+            text="-",
+            font=("Arial",20),
+            command=self.minimizar
+        )
+        self.botao_minimizar.place(relx=0.90, rely=0.0, relwidth=0.05, relheight=1)
+
+        self.botao_sair = ctk.CTkButton(self.frame_topo, text="x",font=("Arial",20), fg_color="red", hover_color="darkred", command=self.sair)
+        self.botao_sair.place(relx=0.95, rely=0.0, relwidth=0.05, relheight=1)
+
+    def minimizar(self):
+        self.iconify()
+
+    def sair(self):
+        self.destroy()
+
+        
+    
+    def iniciar(self):
+        print("iniciando drone")
+
+def run():
+    app = App()
+    app.mainloop()
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import metrics
 import gui
 import simulation
 def main():
+    gui.run()
     pass
 if __name__ == "__main__":
     main()
